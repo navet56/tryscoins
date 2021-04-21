b4522be8e64e6496b53d0117c4e37ce087fe9d4e
Navet56 <evandib@gmail.com>
2021-04-21 11:02:38 +0200

    release 1.1

--- src/GameOver.gd
+++ src/GameOver.gd
@@ -2,3 +2,7 @@ extends Node2D
 
 func _on_MenuButton_pressed():
 	get_tree().change_scene("res://MainMenu.tscn")
+	
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		get_tree().change_scene("res://MainMenu.tscn")
