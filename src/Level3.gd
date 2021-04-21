e3ea56e53008e88a8cc92965969f1a5443451b5f
Navet56 <evandib@gmail.com>
2021-04-21 11:25:00 +0200

    AJout du niveau 3 (vide) et renommage de l'ecran de fin

---
+++ src/Level3.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+func _ready():
+	if $AudioStreamPlayer.playing == false:
+		$AudioStreamPlayer.play()
+		
+		
+func game_over():
+	get_tree().change_scene("res://GameOver.tscn")
+	queue_free()
+	
+func nextLevel():
+	get_tree().change_scene("res://Win.tscn")
+	queue_free()
